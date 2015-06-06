1458a3492d870614a40047f001c1da8c0df0ef48
Todd Lang <todd.lang@kiyote.ca>
2015-06-06 18:27:03 -0400

    Adding missing files.

---
+++ Assets/Script/Events.cs
@@ -0,0 +1,139 @@
+﻿
+using UnityEngine;
+using System;
+using View.Map;
+using Model.Map;
+
+#region System Events
+
+public class InitializeSystemEvent : EventArgs
+{
+}
+
+public class CameraUpdateEvent : EventArgs
+{
+    private readonly Rect _bounds;
+
+    public CameraUpdateEvent(Rect bounds)
+        : base()
+    {
+        _bounds = bounds;
+    }
+
+    public Rect Bounds
+    {
+        get
+        {
+            return _bounds;
+        }
+    }
+}
+
+public class TerrainParsedEvent : EventArgs
+{
+    public TerrainParsedEvent(TerrainTextureDefinition terrainTextureDefinition)
+    {
+        TerrainTextureDefinition = terrainTextureDefinition;
+    }
+
+    public TerrainTextureDefinition TerrainTextureDefinition;
+}
+
+#endregion
+
+#region UI Events
+
+public class MainMenuEvent : EventArgs
+{
+    private bool _show;
+
+    public MainMenuEvent(bool show)
+    {
+        _show = show;
+    }
+
+    public bool Show
+    {
+        get
+        {
+            return _show;
+        }
+    }
+}
+
+public class NewGameMenuEvent : EventArgs
+{
+    private bool _show;
+
+    public NewGameMenuEvent(bool show)
+    {
+        _show = show;
+    }
+
+    public bool Show
+    {
+        get
+        {
+            return _show;
+        }
+    }
+}
+#endregion
+
+#region Game Events
+
+public class CreateGameEvent : EventArgs
+{
+    private int _width;
+    private int _height;
+
+    public CreateGameEvent(int mapWidth, int mapHeight)
+    {
+        _width = mapWidth;
+        _height = mapHeight;
+    }
+
+    public int MapWidth
+    {
+        get
+        {
+            return _width;
+        }
+    }
+
+    public int MapHeight
+    {
+        get
+        {
+            return _height;
+        }
+    }
+}
+
+public class CreateMapEvent : EventArgs
+{
+    private Map _map;
+
+    public CreateMapEvent(Map map)
+    {
+        _map = map;
+    }
+
+    public Map Map
+    {
+        get
+        {
+            return _map;
+        }
+    }
+}
+
+public class StartNewGameEvent : EventArgs
+{
+}
+
+public class TerminateEvent : EventArgs
+{
+}
+
+#endregion
---
+++ Assets/Script/MessageBus.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+public delegate void EventHandler<TEventArgs>(object sender, TEventArgs eventArgs) where TEventArgs : EventArgs;
+
+public class MessageBus
+{
+#if WEAK_MESSAGEBUS
+    private readonly Dictionary<Type, List<WeakReference>> _eventHandlers;
+    private readonly List<WeakReference> _purge;
+#else
+    private readonly Dictionary<Type, List<object>> _eventHandlers;
+#endif
+
+    private static object _lock = new object();
+    private static MessageBus _instance;
+
+    public MessageBus()
+    {
+#if WEAK_MESSAGEBUS
+        _eventHandlers = new Dictionary<Type, List<WeakReference>>();
+        _purge = new List<WeakReference>();
+#else
+        _eventHandlers = new Dictionary<Type, List<object>>();
+#endif
+    }
+
+    public static MessageBus Get()
+    {
+        lock(_lock)
+        {
+            if (_instance == null)
+            {
+                _instance = new MessageBus();
+            }
+
+            return _instance;
+        }
+    }
+
+#if WEAK_MESSAGEBUS
+    private List<WeakReference> GetHandlers<T>()
+    {
+        List<WeakReference> handlers;
+        Type eventType = typeof(T);
+        if (_eventHandlers.ContainsKey(eventType) == false)
+        {
+            handlers = new List<WeakReference>();
+            _eventHandlers.Add(eventType, handlers);
+        }
+        else
+        {
+            handlers = _eventHandlers[eventType];
+        }
+
+        return handlers;
+    }
+#else
+    private List<object> GetHandlers<T>()
+    {
+        List<object> handlers;
+        Type eventType = typeof(T);
+        if (_eventHandlers.ContainsKey(eventType) == false)
+        {
+            handlers = new List<object>();
+            _eventHandlers.Add(eventType, handlers);
+        }
+        else
+        {
+            handlers = _eventHandlers[eventType];
+        }
+
+        return handlers;
+    }
+#endif
+
+    public void Subscribe<T>(EventHandler<T> eventHandler) where T : EventArgs
+    {
+#if WEAK_MESSAGEBUS
+        GetHandlers<T>().Add(new WeakReference(eventHandler));
+#else
+        GetHandlers<T>().Add(eventHandler);
+#endif
+    }
+
+    public void Unsubscribe<T>(EventHandler<T> eventHandler) where T : EventArgs
+    {
+#if WEAK_MESSAGEBUS
+        List<WeakReference> handlers = GetHandlers<T>();
+        foreach (WeakReference r in handlers)
+        {
+            if (ReferenceEquals(r.Target, eventHandler))
+            {
+                handlers.Remove(r);
+                break;
+            }
+        }
+#else
+        List<object> handlers = GetHandlers<T>();
+        foreach (object handler in handlers)
+        {
+            if (ReferenceEquals(handler, eventHandler))
+            {
+                handlers.Remove(handler);
+                break;
+            }
+        }
+#endif
+
+    }
+
+    public void Publish<T>(object sender, T eventArgs) where T : EventArgs
+    {
+#if WEAK_MESSAGEBUS
+        List<WeakReference> handlers = GetHandlers<T>();
+        foreach (WeakReference r in handlers)
+        {
+            if (r.IsAlive)
+            {
+                var eventHandler = r.Target as EventHandler<T>;
+                eventHandler(sender, eventArgs);
+            }
+            else
+            {
+                _purge.Add(r);
+            }
+        }
+
+        if (_purge.Count > 0)
+        {
+            foreach (WeakReference r in _purge)
+            {
+                handlers.Remove(r);
+            }
+            _purge.Clear();
+        }
+#else
+        List<object> handlers = GetHandlers<T>();
+        foreach (object handler in handlers)
+        {
+            var eventHandler = handler as EventHandler<T>;
+            eventHandler(sender, eventArgs);
+        }
+#endif
+    }
+}
